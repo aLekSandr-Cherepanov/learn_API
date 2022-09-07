d099bf9a305f5a891ae031c2b29475241133db99
Alexander Cherepanov <super.sany96@yandex.ru>
2022-09-07 14:01:03 +0300

    закончила с написанием скрипта

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ fetch(
 
     function setHeroes({squadName, homeTown, formed, members}) {
         document.body.insertAdjacentHTML('afterbegin', `
-            <h1>#{}</h1>
-        `)
+            <h1>${squadName}</h1>
+            <h2>Hometown: ${homeTown} // Formed: ${formed}</h2>
+            <div class="heroes">${setMembers(members)}</div>
+        `);
+    }
+
+    function setMembers(members) {
+        return members.map(
+            (hero) => `
+            <h3>${hero.name}</h3>
+            <p>Secret indentity: ${hero.secretIdentity}</p>
+            <p>Age: ${hero.age}</p>
+            <p>Superpowers</p>
+            <ul>
+                ${hero.powers.map((power) => `<li>${power}</li>`).join(' ')}
+            </ul>
+        `
+        )
+        .join(' ');
     }
