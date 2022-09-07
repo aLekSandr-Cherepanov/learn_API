6a9b02fefbf5c180cb768f0c98605f645fa0cbc8
Alexander Cherepanov <super.sany96@yandex.ru>
2022-09-07 11:43:07 +0300

    написание js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello')
+fetch(
+    'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json'
+    )
+    .then((res) => res.json())
+    .then((data) => setHeroes(data));
+
+    function setHeroes({squadName, homeTown, formed, members}) {
+        document.body.insertAdjacentHTML('afterbegin', `
+            <h1>#{}</h1>
+        `)
+    }
